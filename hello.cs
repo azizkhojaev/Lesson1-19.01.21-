135c31dd6c4d2ff1e4529a114c1e0a22e476b7d4
Fazliddin <fazliddinazizkhojaev@gmail.com>
2021-01-20 16:39:24 +0500

    zx

---
+++ hello.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("hello somon");
+        }
+    }
+}
